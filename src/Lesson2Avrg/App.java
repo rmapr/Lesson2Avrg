f6df8f00454151fa1d9b46e270a29aa4f7bb8eb7
rmapr <rmap@ukr.net>
2023-05-03 15:49:19 +0300

    Declare variables, "Skeleton") application

--- src/Lesson2Avrg/App.java
+++ src/Lesson2Avrg/App.java
@@ -1,4 +1,38 @@
 package Lesson2Avrg;
 
+import java.util.Scanner;
+
 public class App {
+    static Scanner sc;
+    static int numTask;
+    public static void main(String[] args) {
+        numTask = getNumTask();
+        switch (numTask) {
+            case 1 -> {
+//                calcAvrgTwoNumbers();
+            }
+            case 2-> {
+//                calcAvrgAnyNumbers();
+            }
+            default ->{
+                System.out.println("Shutdown...");
+                sc.close();
+            }
+        }
+    }
+    private static double getAvrgNumbers(double sum, double number){
+        return sum / number;
+    }
+
+    private static int getNumTask(){
+        printMenu();
+        sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+    private static void printMenu(){
+        System.out.print("Enter task number (1,2 or 0):\n" +
+                "1: Arithmetic mean of two numbers;\n" +
+                "2: Arithmetic mean of an arbitrary number of numbers\n" +
+                "0: Exit.\n");
+    }
 }
